fea6cc789892da0a268dfbffa00c0f08bcc33cf8
0Clark30 <austinlaw.clark@outlook.com>
2024-01-10 13:39:18 -0600

    added questions object for questionaire

---
+++ assets/js/script.js
@@ -0,0 +1,182 @@
+var questionsObject = [
+    {
+        question: "1. I’m interested in adopting a ____. (Dropdown)",
+        dropdownOptions: [
+            "Dog",
+            "Cat"
+        ]
+    },
+    {
+        question: "2. I am wanting to adopt for _____.",
+        dropdownOptions: [
+            "Myself",
+            "My family"
+        ]
+    },
+    {
+        question: "3. Who lives with you?",
+        dropdownOptions: [
+            "Single",
+            "Children",
+            "Married",
+            "Roommates"
+        ]
+    },
+    {
+        question: "3. I am a _____ pet owner.",
+        dropdownOptions: [
+            "First-time",
+            "Previous",
+            "Current"
+        ]
+    },
+    {
+        question: "4. I currently have _____.",
+        dropdownOptions: [
+            "No dog(s) or cat(s)",
+            "Cat(s)",
+            "Dog(s)",
+            "Dog(s) and cat(s)"
+        ]
+    },
+    {
+        question: "5. My home has ____.",
+        dropdownOptions: [
+            "No yard",
+            "A fenced yard",
+            "A yard",
+            "An outdoor area",
+            "A nearby park"
+        ]
+    },
+    {
+        question: "6. How much free time do you have to devote to a pet?",
+        dropdownOptions: [
+            "30 minutes a day",
+            "2 hours a day",
+            "4 hours a day",
+            "More than 4 hours a day"
+        ]
+    },
+    {
+        question: "7. There are _____ as part of my HOA or lease.",
+        dropdownOptions: [
+            "Dog restrictions",
+            "No dog restrictions"
+        ]
+    },
+    {
+        question: "8. My HOA or lease will not allow a dog that is ____.",
+        dropdownOptions: [
+            "No size restriction",
+            "Over 25 lbs",
+            "Over 60 lbs",
+            "Over 100 lbs"
+        ]
+    },
+    {
+        question: "9. My ideal dog (or cat) is _____.",
+        dropdownOptions: [
+            "No age preference",
+            "A puppy/kitten",
+            "A young pet (1-6 years)",
+            "A senior pet (6+ years)"
+        ]
+    },
+    {
+        question: "10. I would like to adopt _____.",
+        dropdownOptions: [
+            "Female",
+            "Male",
+            "(No gender preference)"
+        ]
+    },
+    {
+        question: "11. I prefer a dog that is _____.",
+        dropdownOptions: [
+            "No size preference",
+            "Small (0-25 lbs)",
+            "Medium (26-60 lbs)",
+            "Large (61-100 lbs)",
+            "Extra large (101 lbs or more)"
+        ]
+    },
+    {
+        question: "12. Are you comfortable with giving medication to your pet?",
+        dropdownOptions: [
+            "Yes",
+            "No"
+        ]
+    },
+    {
+        question: "13. My future pet must be _____.",
+        dropdownOptions: [
+            "Allergy-friendly",
+            "House-trained"
+        ]
+    },
+    {
+        question: "14. What is your optimal pet budget?",
+        dropdownOptions: [
+            "$755 annually (cat answer)",
+            "$1,118 annually (dog answer)",
+            "$3,145 annually (dog and cat answer)",
+            "$4,095 annually (dog answer)"
+        ]
+    },
+    {
+        question: "15. How much time can you devote to cleaning after your pet?",
+        dropdownOptions: [
+            "30 minutes a day (cat answer)",
+            "1 hour a day (dog answer)",
+            "2 hours a day (dog answer)"
+        ]
+    },
+    {
+        question: "16. How long can you allocate to play with your new pet?",
+        dropdownOptions: [
+            "30 minutes a day (cat answer)",
+            "1 hour a day (dog answer)",
+            "3 hours a day (dog answer)"
+        ]
+    },
+    {
+        question: "17. What level of shedding for your pet are you looking for?",
+        dropdownOptions: ["a.1", "b.2", "c.3", "d.4", "e.5"]
+    },
+    {
+        question: "18. What friendliness level best suits your needs for your future pet?",
+        dropdownOptions: ["a.1", "b.2", "c.3", "d.4", "e.5"]
+    },
+    {
+        question: "19. Is there a particular breed you are looking for? (Drop down/ open user input - check spelling)"
+    },
+    {
+        question: "20. What level of playfulness is best for you and your pet?",
+        dropdownOptions: ["a.1", "b.2", "c.3", "d.4", "e.5"]
+    },
+    {
+        question: "21. What level of trainability are you looking for?",
+        dropdownOptions: ["a.1", "b.2", "c.3", "d.4", "e.5"]
+    },
+    {
+        question: "22. What energy level is the best fit for you?",
+        dropdownOptions: ["a.1", "b.2", "c.3", "d.4", "e.5"]
+    },
+    {
+        question: "23. How important is it that the pet is children-friendly?",
+        dropdownOptions: ["a.1", "b.2", "c.3", "d.4", "e.5"]
+    },
+    {
+        question: "24. What level of grooming is most comfortable for you?",
+        dropdownOptions: ["a.1", "b.2", "c.3", "d.4", "e.5"]
+    },
+    {
+        question: "25. What level of barking is acceptable to you?",
+        dropdownOptions: ["a.1", "b.2", "c.3", "d.4", "e.5"]
+    },
+    {
+        question: "26. Are you comfortable with owning a pet with special needs?",
+        dropdownOptions: ["a. Yes", "b. No"]
+    }
+];
