8fa342c0eef7e2c5c08efe09f90f26d1b2140b55
Thomas Tran <2001thomas.tran@gmail.com>
2024-01-10 13:19:43 -0600

    added ninjaAPI

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var APINinjas = 'cjMugAovo7SNn+DtczTAMg==P49uxlggz9Z3SFRx'
+
+var requestNinjaCats = 'https://api.api-ninjas.com/v1/${petType}?name='
+
+function petType() {
+    if () {
+
+    }
+}
+function searchApi(query, format) {
+    var 
+}
